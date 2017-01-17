b250ff060c299f2b080d98053f524da41ddeac7e
booster <booster@BOOSTER-PC>
2017-01-17 23:00:49 +0300

    Changed indentation

--- ProductQuery.sql
+++ ProductQuery.sql
@@ -1,7 +1,7 @@
 SELECT t.ProductId, COUNT(1) FirstCount
 FROM
-(SELECT ProductId, ROW_NUMBER() OVER (PARTITION BY CustomerId ORDER BY DateCreated) rowNum
-FROM Sales) t
+	(SELECT ProductId, ROW_NUMBER() OVER (PARTITION BY CustomerId ORDER BY DateCreated) rowNum
+	FROM Sales) t
 WHERE t.rowNum=1
 GROUP BY t.ProductId
 
